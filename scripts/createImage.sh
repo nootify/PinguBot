ee6ba460f9505a6c96f6fcab3dda2917959a45b9
popcor255 <popcor255@gmail.com>
2020-06-30 21:50:27 -0400

    put script in scripts folder

--- scripts/createImage.sh
+++ scripts/createImage.sh
@@ -8,5 +8,7 @@ if ! [ -x "$(command -v gzip)" ]; then
   exit 1
 fi
 
+cp -v .gitignore .dockerignore
 docker build -t bots/pingubot-build:latest .
 docker save bots/pingubot-build:latest | gzip > pingubot-build.tar.gz
+rm -fr .dockerignore
