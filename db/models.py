cb0314295b4c29e4c6eaeba4ae995661ee6d1cc1
nootify <48371116+nootify@users.noreply.github.com>
2021-05-22 20:08:54 -0400

    Add default value to nomination_date

--- db/models.py
+++ db/models.py
@@ -6,5 +6,5 @@ class Clown(db.Model):
     guild_id = db.Column(db.Numeric, primary_key=True)
     clown_id = db.Column(db.Numeric)
     previous_clown_id = db.Column(db.Numeric)
-    nomination_date = db.Column(db.Date)
+    nomination_date = db.Column(db.Date(), server_default="now()")
     join_time = db.Column(db.DateTime)
