270a344327e010d19e1e63530d19e7bf76cab58b
nootify <48371116+nootify@users.noreply.github.com>
2021-05-24 17:19:13 -0400

    Fix start script messages

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# Ensures the bot does not launch until the database is running
+# Wait until both services are up and receiving connections
 while ! nc -z "db" 5432 ; do
     sleep 1;
-    echo "Waiting for PostgreSQL..."
+    echo "Waiting for PostgreSQL ..."
 done
 while ! nc -z "audio" 2333 ; do
     sleep 1;
-    echo "Waiting for Lavalink..."
+    echo "Waiting for Lavalink ..."
 done
 python /pingubot/pingu.py
