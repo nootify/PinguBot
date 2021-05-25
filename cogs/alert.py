b1df8e7afc02c4466350002fa8e580fadda729bf
nootify <48371116+nootify@users.noreply.github.com>
2021-05-25 01:14:27 -0400

    Refactor alert module with cleaner command structure

--- cogs/alert.py
+++ cogs/alert.py
@@ -6,93 +6,96 @@ import discord
 from discord.ext import commands, tasks
 from pytz import timezone
 
+from common.util import Icons
+
 
 class Alert(commands.Cog):
+    """test"""
+
     def __init__(self, bot):
         self.bot = bot
         self.log = logging.getLogger(__name__)
-        self.message_location = None  # Singular location
+        self.message_location = None
         self.sent_message = None
 
-    async def cog_check(self, ctx):
-        return await self.bot.is_owner(ctx.author)
+    # async def cog_check(self, ctx):
+    #     return await self.bot.is_owner(ctx.author)
 
     def cog_unload(self):
-        self.log.info("Cog unloaded; stopping 1234 and resetting message location to nowhere")
-        self.message_location = None
-        self.sent_message = None
         self.message_1234.cancel()
 
-    @commands.command(name="1234", hidden=True)
-    async def set_1234(self, ctx, guild_id: str = None, channel_id: str = None):
-        # Temporary variable to compare with the cog level location
-        preliminary_location = None
+    def get_snipe_channel(self, guild: discord.Guild, channel_id: str) -> discord.TextChannel:
+        try:
+            channel = guild.get_channel(int(channel_id))
+        except ValueError:
+            raise commands.BadArgument("Not a valid channel id.")
+        if not channel:
+            raise commands.BadArgument("Channel with this id in the guild does not exist.")
+        if channel.type != discord.ChannelType.text:
+            raise commands.BadArgument(f"Cannot snipe a {channel.type} channel.")
 
-        # Parse the location to send the message
-        if not guild_id:
-            self.log.info("No location specified; sending in current channel")
-            preliminary_location = ctx.message.channel
-            # await ctx.send(
-            #     f"{self.bot.icons['info']} No IDs specified. Sending message in current channel.")
-        elif not channel_id:
-            raise commands.BadArgument("Missing channel ID to send in server.")
+        self.log.info("Sniping on channel: %s", channel.id)
+        return channel
 
-        # Locate requested server and channel
-        if not preliminary_location:
-            # Error checking if given IDs are not random strings
-            try:
-                guild_id = int(guild_id)
-                channel_id = int(channel_id)
-            except ValueError as exc:
-                self.log.error("Failed to parse server ID and/or text channel ID")
-                raise commands.BadArgument("Given ID(s) were not a numerical value.") from exc
-
-            # Search for the server using its ID
-            selected_guild = self.bot.get_guild(guild_id)
-            if not selected_guild:
-                self.log.error("Unable to find requested server")
-                raise commands.BadArgument("Unable to find requested server")
-            self.log.info(
-                "Found requested server (id: %s, name: %s)",
-                selected_guild.id,
-                selected_guild.name,
-            )
-
-            # Search for the channel using its ID
-            selected_channel = selected_guild.get_channel(channel_id)
-            if not selected_channel:
-                self.log.error("Unable to find requested channel")
-                raise commands.BadArgument("Specified text channel does not exist or read permission(s) were not given")
-            self.log.info(
-                "Found requested text channel (id: %s, name: %s)",
-                selected_channel.id,
-                selected_channel.name,
-            )
-
-            preliminary_location = selected_channel
-
-        self.message_location = preliminary_location
-
-        # Check the channel permissions
-        self_permissions = self.message_location.permissions_for(ctx.me)
+    def check_snipe_permission(self, ctx: commands.Context, channel: discord.TextChannel):
+        self_permissions = channel.permissions_for(ctx.me)
         if not self_permissions.send_messages:
-            self.log.error("Missing permission to send messages in the specified location")
-            raise commands.BadArgument("Missing permission to send messages in that channel.")
+            self.log.error("Missing permission to send messages")
+            raise commands.BadArgument("Missing permission to send messages.")
 
-        # Restart the task (the following condition will be used when discord.py 1.4.0 is released)
-        if self.message_1234.is_running():  # pylint: disable=no-member
-            self.message_1234.restart()  # pylint: disable=no-member
-        # self.message_1234.cancel() # pylint: disable=no-member
-        # await asyncio.sleep(1)
+    async def start_snipe(self):
+        if self.message_1234.is_running():
+            self.message_1234.restart()
         else:
-            self.message_1234.start()  # pylint: disable=no-member
+            self.message_1234.start()
 
-    @commands.command(name="no1234", hidden=True)
-    async def stop_1234(self, ctx):  # pylint: disable=unused-argument
-        self.log.info("Manual override to stop 1234 received; stopping task")
-        self.message_location = None
-        self.sent_message = None
-        self.message_1234.cancel()  # pylint: disable=no-member
+    @commands.group(name="snipe", hidden=True)
+    @commands.is_owner()
+    async def snipe(self, ctx: commands.Context):
+        """Check the snipe status"""
+        if ctx.invoked_subcommand:
+            return
+
+        # TODO: Add mechanism to get the snipe time
+        await ctx.send(f"{Icons.ALERT} Next snipe will happen in: [TODO: implement]", delete_after=3)
+
+    @snipe.command(name="remote", aliases=["r"])
+    async def remote_snipe(self, ctx: commands.Context, guild_id: str, channel_id: str):
+        try:
+            sniped_guild = self.bot.get_guild(int(guild_id))
+        except ValueError:
+            raise commands.BadArgument("Not a valid guild id.")
+        if not sniped_guild:
+            raise commands.BadArgument("Guild with this id does not exist.")
+
+        sniped_channel = self.get_snipe_channel(sniped_guild, channel_id)
+        self.check_snipe_permission(ctx, sniped_channel)
+
+        self.message_location = sniped_channel
+        await self.start_snipe()
+        await ctx.send(f"{Icons.ALERT} Snipe set to guild/channel: {sniped_guild.id}/{sniped_channel.id}")
+
+    @remote_snipe.error
+    async def remote_snipe_error_handler(self, ctx: commands.Context, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"{Icons.ERROR} Missing `{error.param.name}` to send message to", delete_after=3)
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"{Icons.ERROR} {error}", delete_after=3)
+
+    @commands.command(name="cancelsnipe", aliases=["csnipe"], hidden=True)
+    @commands.is_owner()
+    async def cancel_snipe(self, ctx: commands.Context):
+        if self.message_1234.is_running():
+            self.sent_message = None
+            self.message_1234.cancel()
+            await ctx.send(f"{Icons.SUCCESS} Stopped snipe on channel: {self.message_location.id}")
+        else:
+            raise commands.BadArgument("Task already stopped.")
+
+    @cancel_snipe.error
+    async def cancel_snipe_error_handler(self, ctx: commands.Context, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"{Icons.ERROR} {error}", delete_after=3)
 
     @tasks.loop()
     async def message_1234(self):
@@ -168,7 +171,6 @@ class Alert(commands.Cog):
 
     @message_1234.before_loop
     async def prep_1234(self):
-        """Stop the task from running until Pingu is in the ready state"""
         await self.bot.wait_until_ready()
 
     @commands.Cog.listener()
@@ -195,5 +197,4 @@ class Alert(commands.Cog):
 
 
 def setup(bot):
-    """Adds this module in as a cog to Pingu."""
     bot.add_cog(Alert(bot))
