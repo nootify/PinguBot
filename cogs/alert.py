ffd91619510dfa885110e6143ab340017e869bce
nootify <48371116+nootify@users.noreply.github.com>
2021-05-25 22:22:50 -0400

    Clean up module

--- cogs/alert.py
+++ cogs/alert.py
@@ -1,6 +1,5 @@
-import asyncio
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import discord
 from discord.ext import commands, tasks
@@ -15,14 +14,15 @@ class Alert(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.log = logging.getLogger(__name__)
-        self.message_location = None
+        self.snipe_location = None
+        self.snipe_time = None
         self.sent_message = None
 
     # async def cog_check(self, ctx):
     #     return await self.bot.is_owner(ctx.author)
 
     def cog_unload(self):
-        self.message_1234.cancel()
+        self.send_snipe.cancel()
 
     def get_snipe_channel(self, guild: discord.Guild, channel_id: str) -> discord.TextChannel:
         try:
@@ -37,30 +37,42 @@ class Alert(commands.Cog):
         self.log.info("Sniping on channel: %s", channel.id)
         return channel
 
-    def check_snipe_permission(self, ctx: commands.Context, channel: discord.TextChannel):
-        self_permissions = channel.permissions_for(ctx.me)
+    def check_snipe_permission(self, channel: discord.TextChannel) -> bool:
+        self_permissions = channel.permissions_for(channel.guild.me)
         if not self_permissions.send_messages:
-            self.log.error("Missing permission to send messages")
-            raise commands.BadArgument("Missing permission to send messages.")
+            return False
+        return True
 
-    async def start_snipe(self):
-        if self.message_1234.is_running():
-            self.message_1234.restart()
+    async def setup_snipe(self):
+        if self.send_snipe.is_running():
+            self.send_snipe.restart()
         else:
-            self.message_1234.start()
+            self.send_snipe.start()
 
     @commands.group(name="snipe", hidden=True)
     @commands.is_owner()
-    async def snipe(self, ctx: commands.Context):
+    async def snipe_info(self, ctx: commands.Context):
         """Check the snipe status"""
         if ctx.invoked_subcommand:
             return
 
-        # TODO: Add mechanism to get the snipe time
-        await ctx.send(f"{Icons.ALERT} Next snipe will happen in: [TODO: implement]", delete_after=3)
+        if not self.snipe_location:
+            raise commands.BadArgument("No snipe has been set.")
 
-    @snipe.command(name="remote", aliases=["r"])
+        await ctx.send(
+            f"{Icons.ALERT} Sending snipe to guild '{self.snipe_location.guild.name}'"
+            + f" (id: {self.snipe_location.guild.id}) on channel '{self.snipe_location.name}'"
+            + f" (id: {self.snipe_location.id}) at `{self.snipe_time}`."
+        )
+
+    @snipe_info.error
+    async def snipe_error_handler(self, ctx: commands.Context, error):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(f"{Icons.ERROR} {error}")
+
+    @snipe_info.command(name="remote", aliases=["r"])
     async def remote_snipe(self, ctx: commands.Context, guild_id: str, channel_id: str):
+        """Set the snipe location"""
         try:
             sniped_guild = self.bot.get_guild(int(guild_id))
         except ValueError:
@@ -69,108 +81,84 @@ class Alert(commands.Cog):
             raise commands.BadArgument("Guild with this id does not exist.")
 
         sniped_channel = self.get_snipe_channel(sniped_guild, channel_id)
-        self.check_snipe_permission(ctx, sniped_channel)
+        if not self.check_snipe_permission(sniped_channel):
+            raise commands.BadArgument("Missing permission to send messages.")
 
-        self.message_location = sniped_channel
-        await self.start_snipe()
-        await ctx.send(f"{Icons.ALERT} Snipe set to guild/channel: {sniped_guild.id}/{sniped_channel.id}")
+        self.snipe_location = sniped_channel
+        await self.setup_snipe()
+        await ctx.send(
+            f"{Icons.ALERT} Sending snipe to guild '{sniped_guild.name}' (id: {sniped_guild.id})"
+            + f" on channel '{sniped_channel.name}' (id: {sniped_channel.id})."
+        )
 
     @remote_snipe.error
     async def remote_snipe_error_handler(self, ctx: commands.Context, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"{Icons.ERROR} Missing `{error.param.name}` to send message to", delete_after=3)
+            await ctx.send(f"{Icons.ERROR} Missing `{error.param.name}` to send message to")
         if isinstance(error, commands.BadArgument):
-            await ctx.send(f"{Icons.ERROR} {error}", delete_after=3)
+            await ctx.send(f"{Icons.ERROR} {error}")
 
     @commands.command(name="cancelsnipe", aliases=["csnipe"], hidden=True)
     @commands.is_owner()
     async def cancel_snipe(self, ctx: commands.Context):
-        if self.message_1234.is_running():
-            self.sent_message = None
-            self.message_1234.cancel()
-            await ctx.send(f"{Icons.SUCCESS} Stopped snipe on channel: {self.message_location.id}")
+        """Cancel the snipe if it exists"""
+        if self.send_snipe.is_running():
+            await ctx.send(f"{Icons.SUCCESS} Stopped snipe")
+            self.send_snipe.cancel()
         else:
-            raise commands.BadArgument("Task already stopped.")
+            raise commands.BadArgument("Snipe not active.")
 
     @cancel_snipe.error
     async def cancel_snipe_error_handler(self, ctx: commands.Context, error):
         if isinstance(error, commands.BadArgument):
-            await ctx.send(f"{Icons.ERROR} {error}", delete_after=3)
+            await ctx.send(f"{Icons.ERROR} {error}")
 
     @tasks.loop()
-    async def message_1234(self):
-        # Python times, timezones, and daylight savings is hard
-        utc = timezone("UTC")
-        # Account for daylight savings ("US/Eastern" is EST only)
-        eastern = timezone("America/New_York")
-        now = utc.localize(datetime.utcnow())
-        if self.message_location:
-            today_12am = eastern.localize(datetime(now.year, now.month, now.day, 0, 34))
-            today_12pm = eastern.localize(datetime(now.year, now.month, now.day, 12, 34))
-            tomorrow_12am = eastern.localize(datetime(now.year, now.month, now.day + 1, 0, 34))
+    async def send_snipe(self):
+        if self.snipe_location:
+            # Account for daylight savings ("US/Eastern" is EST only)
+            adjusted_timezone = timezone("America/New_York")
+            utc = timezone("UTC")
 
-            debug_mode = False
-            if debug_mode:
-                wait_this_long = 60
-                self.log.info(
-                    "UTC: %s | Eastern: %s",
-                    now.astimezone(utc),
-                    now.astimezone(eastern),
-                )
-                self.log.info(
-                    "UTC: %s | Eastern: %s",
-                    today_12am.astimezone(utc),
-                    today_12am.astimezone(eastern),
-                )
-                self.log.info(
-                    "UTC: %s | Eastern: %s",
-                    today_12pm.astimezone(utc),
-                    today_12pm.astimezone(eastern),
-                )
-                self.log.info(
-                    "UTC: %s | Eastern: %s",
-                    tomorrow_12am.astimezone(utc),
-                    tomorrow_12am.astimezone(eastern),
-                )
-                self.log.warning("DEBUG MODE ENABLED: Firing in the next %s seconds", wait_this_long)
-                await asyncio.sleep(wait_this_long)
-            elif now <= today_12am:
-                wait = (today_12am - now).total_seconds()
-                self.log.info(
-                    "Sending message at 12:34 AM (%s left)",
-                    str(timedelta(seconds=round(wait))),
-                )
-                await discord.utils.sleep_until(today_12am)
-            elif now <= today_12pm:
-                wait = (today_12pm - now).total_seconds()
-                self.log.info(
-                    "Sending message at 12:34 PM (%s left)",
-                    str(timedelta(seconds=round(wait))),
-                )
-                await discord.utils.sleep_until(today_12pm)
+            now = datetime.now(adjusted_timezone)
+            today_midnight = adjusted_timezone.localize(datetime(now.year, now.month, now.day, 0, 34))
+            today_noon = adjusted_timezone.localize(datetime(now.year, now.month, now.day, 12, 34))
+            tomorrow_midnight = adjusted_timezone.localize(datetime(now.year, now.month, now.day + 1, 0, 34))
+
+            if now <= today_midnight:
+                wait_time = today_midnight
+            elif now <= today_noon:
+                wait_time = today_noon
             else:
-                wait = (tomorrow_12am - now).total_seconds()
-                self.log.info(
-                    "Sending message at 12:34 AM tomorrow (%s left)",
-                    str(timedelta(seconds=round(wait))),
-                )
-                await discord.utils.sleep_until(tomorrow_12am)
+                wait_time = tomorrow_midnight
 
-        # Things could have happened in the meantime, so check again
-        if self.bot.get_channel(self.message_location.id):
-            self.log.info(
-                "Sending message to text channel (id: %s, name: %s)",
-                self.message_location.id,
-                self.message_location.name,
+            self.snipe_time = wait_time
+            self.log.debug(
+                "Sending snipe at '%s' local time ('%s' utc)",
+                self.snipe_time,
+                self.snipe_time.astimezone(utc),
             )
-            self.sent_message = await self.message_location.send("12:34")
-        else:
-            self.log.error("Channel disappeared before message was sent; stopping task")
-            self.message_location = None
-            self.message_1234.cancel()
+            self.log.debug(
+                "now: %s, today 12am: %s, today 12pm: %s, tomrrow 12am: %s",
+                now,
+                today_midnight,
+                today_noon,
+                tomorrow_midnight,
+            )
+            await discord.utils.sleep_until(wait_time)
 
-    @message_1234.before_loop
-    async def prep_1234(self):
+            # Something could have happened in the meantime, so check the channel
+            if self.bot.get_channel(self.snipe_location.id) and self.check_snipe_permission(self.snipe_location):
+                self.sent_message = await self.snipe_location.send("12:34")
+            else:
+                self.log.error("Channel is inaccessible; stopping snipe")
+                self.snipe_location = None
+                self.snipe_time = None
+                self.sent_message = None
+                self.send_snipe.cancel()
+
+    @send_snipe.before_loop
+    async def prep_snipe(self):
         await self.bot.wait_until_ready()
 
     @commands.Cog.listener()
