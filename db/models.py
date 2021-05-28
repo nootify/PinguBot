1ded6790b7d4173cd8681a878595a9837ad1bc0a
nootify <48371116+nootify@users.noreply.github.com>
2021-05-28 04:35:06 -0400

    Fix nomination_date default value and add Reminder schema

--- db/models.py
+++ db/models.py
@@ -6,5 +6,14 @@ class Clown(db.Model):
     guild_id = db.Column(db.Numeric, primary_key=True)
     clown_id = db.Column(db.Numeric)
     previous_clown_id = db.Column(db.Numeric)
-    nomination_date = db.Column(db.Date(), server_default="now()")
+    nomination_date = db.Column(db.Date(), server_default=db.func.now(), onupdate=db.func.now())
     join_time = db.Column(db.DateTime)
+
+
+class Reminder(db.Model):
+    __tablename__ = "reminders"
+    reminder_id = db.Column(db.Integer(), primary_key=True)
+    reminder_text = db.Column(db.Unicode)
+    reminder_time = db.Column(db.DateTime)
+    user_id = db.Column(db.Numeric)
+    channel_id = db.Column(db.Numeric)
