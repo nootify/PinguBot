ef88c9f2b2f655e6faa6fba355c00ffdce9d3539
nootify <48371116+nootify@users.noreply.github.com>
2022-02-28 20:44:20 -0500

    Remove unused lavalink vars and fix jishaku loading bug

--- common/utils.py
+++ common/utils.py
@@ -24,16 +24,8 @@ class Settings:
     LAVALINK_HOST = os.environ.get("LAVALINK_HOST")
     LAVALINK_PORT = int(os.environ.get("LAVALINK_PORT"))
     LAVALINK_PASSWORD = os.environ.get("LAVALINK_PASSWORD")
-    LAVALINK_REGION = os.environ.get("LAVALINK_REGION")
 
-    POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
-    POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
-    POSTGRES_DB = os.environ.get("POSTGRES_DB")
-    POSTGRES_USER = os.environ.get("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-    POSTGRES_URL = (
-        f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    )
+    POSTGRES_URL = os.environ.get("POSTGRES_URL")
 
     VERSION = "1.0"
 
