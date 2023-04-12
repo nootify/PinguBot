d94bcd89c9e372fed8a972233be713cd46d80c87
nootnoot <48371116+nootify@users.noreply.github.com>
2023-04-12 19:39:46 -0400

    Fix db connection init

--- models/__init__.py
+++ models/__init__.py
@@ -29,9 +29,13 @@ class Reminder(Base):
     channel_id = Column(BigInteger)
 
 
-database_url = os.environ.get("DATABASE_URL")
-if database_url and database_url.startswith("postgres://"):
-    database_url = database_url.replace("postgres://", "postgresql+asyncpg://", 1)
+user = os.environ.get("POSTGRES_USER")
+pw = os.environ.get("POSTGRES_PASSWORD")
+db_host = os.environ.get("POSTGRES_HOST")
+db_port = os.environ.get("POSTGRES_PORT")
+database_url = os.environ.get("DATABASE_URL") or f"postgresql+asyncpg://{user}:{pw}@{db_host}{db_port}/{user}"
+if database_url and database_url.startswith("postgresql://"):
+    database_url = database_url.replace("postgresql://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(
     database_url,
